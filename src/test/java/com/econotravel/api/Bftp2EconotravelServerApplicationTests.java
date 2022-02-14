6ee89fa096ac4c442c8d933ae012b7a43540ee5e
FabyBroc <fabybroc@hotmail.com>
2022-02-14 10:10:35 +0100

    revisando tests

--- src/test/java/com/econotravel/api/Bftp2EconotravelServerApplicationTests.java
+++ src/test/java/com/econotravel/api/Bftp2EconotravelServerApplicationTests.java
@@ -9,11 +9,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -83,47 +86,45 @@ class Bftp2EconotravelServerApplicationTests {
     }
 
     @Test
-    void allowsToCreateANewExperience() throws Exception {
-        mockMvc.perform(post("/new")
-                        .param("name", "Paseo en bicicleta por el Montseny")
-                        .param("price", String.valueOf(250))
-                        .param("time", String.valueOf(5))
-                        .param("category", "Montaña, bicicleta, excursión larga")
-                        .param("imageUrl", "")
+    void allowsToEditAnExperience() throws Exception {
+        Experience experience = experienceRepository.save(new Experience("Descubre la costa en barco de vela", "../img/barcoDeVela.png", "Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona. Una escapada veraniega apta incluso para los días más calurosos del año. Descubre los encantadores alrededores de la ciudad de Barcelona y visita desde el mar sus más impresionantes playas y calas. Comenzaremos la excursión en el Puerto de Barcelona, desde donde partiremos hacia el norte para visitar playas como la Mar Bella, la Playa de la Mora y la Playa de los Pescadores. A bordo de la embarcación podremos disfrutar de una selección de quesos y embutidos catalanes acompañada de cava brut y zumos de frutas. Asimismo, pararemos cerca de la Playa de Montgat para realizar una actividad de buceo de superficie que nos permitirá apreciar la diversidad de la fauna marítima local y su ecosistema. Finalizaremos la excursión en el mismo puerto del que partimos.",
+                280,
+                5,
+                "Actividad disponible para todas las edades. Pasarela para silla de ruedas disponible bajo reserva.",
+                "Playa, barco, excursión larga."));
 
-                )
-                .andExpect(status().is(200));
+        mockMvc.perform(put("/api/experiences/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\": \"" + experience.getId() + "\", \"name\": \"Descubre la costa de Barcelona\"," + " \"image\": \"../img/montseny.png\"," +
+                        " \"description\": \"Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona\"," +
+                        "\"price\": 200," +
+                        "\"duration\": \"2h\"," +
+                        "\"tags\": \"Excursión larga\"}")
+        ).andExpect(status().isOk());
 
-        List<Experience> existingExperiences = (List<Experience>) experienceRepository.findAll();
-        assertThat(existingExperiences, contains(allOf(
-                hasProperty("name", equalTo("Paseo en bicicleta por el Montseny")),
-                hasProperty("price", equalTo(250)),
-                hasProperty("time", equalTo(5)),
-                hasProperty("category", equalTo("Montaña, bicicleta, excursión larga")),
-                hasProperty("imageUrl", equalTo(""))
-        )));
+
+        List<Experience> experiences = experienceRepository.findAll();
+
+        assertThat(experiences, hasSize(1));
+        assertThat(experiences.get(0).getName(), equalTo("Descubre la costa de Barcelona"));
+        assertThat(experiences.get(0).getImgUrl(), equalTo("../img/montseny.png"));
+        assertThat(experiences.get(0).getDescripcion(), equalTo("Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona"));
+        assertThat(experiences.get(0).getPrice(), equalTo(200.0));
+        assertThat(experiences.get(0).getTime(), equalTo("2h"));
+        assertThat(experiences.get(0).getCategory(), equalTo("Excursión larga"));
+
+
     }
 
     @Test
-    void allowsToEditExistentExperience() throws Exception {
-        mockMvc.perform(post("/api/experiences/edit")
-                        .param("name", "Paseo en bicicleta por el Montseny")
-                        .param("price", String.valueOf(250))
-                        .param("time", String.valueOf(5))
-                        .param("category", "Montaña, bicicleta, excursión larga")
-                        .param("imageUrl", "https://cdn2.civitatis.com/espana/viladrau/tour-bicicleta-electrica-parque-montseny-grid.jpg")
-
-                )
+    void allowsToDeleteAnExperience() throws Exception {
+        Experience experience = experienceRepository.save(new Experience("Descubre la costa en barco de vela", "../img/montseny.png", "Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona. Una escapada veraniega apta incluso para los días más calurosos del año. Descubre los encantadores alrededores de la ciudad de Barcelona y visita desde el mar sus más impresionantes playas y calas. Comenzaremos la excursión en el Puerto de Barcelona, desde donde partiremos hacia el norte para visitar playas como la Mar Bella, la Playa de la Mora y la Playa de los Pescadores. A bordo de la embarcación podremos disfrutar de una selección de quesos y embutidos catalanes acompañada de cava brut y zumos de frutas. Asimismo, pararemos cerca de la Playa de Montgat para realizar una actividad de buceo de superficie que nos permitirá apreciar la diversidad de la fauna marítima local y su ecosistema. Finalizaremos la excursión en el mismo puerto del que partimos.", 280, 5, "Actividad disponible para todas las edades. Pasarela para silla de ruedas disponible bajo reserva.", ""));
+        mockMvc.perform(delete("/api/experiences" + experience.getId()))
                 .andExpect(status().is(200));
 
-        List<Experience> existingExperiences = (List<Experience>) experienceRepository.findAll();
-        assertThat(existingExperiences, contains(allOf(
-                hasProperty("name", equalTo("Paseo en bicicleta por el Montseny")),
-                hasProperty("price", equalTo(250)),
-                hasProperty("time", equalTo(5)),
-                hasProperty("category", equalTo("Montaña, bicicleta, excursión larga")),
-                hasProperty("imageUrl", equalTo("https://cdn2.civitatis.com/espana/viladrau/tour-bicicleta-electrica-parque-montseny-grid.jpg"))
-        )));
+        assertThat(experienceRepository.findById(experience.getId()), equalTo(Optional.empty()));
     }
+
+
 
 }
