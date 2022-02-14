9a6e9d5bf63c174da23f7fd8a0dedfc5844f0810
FabyBroc <fabybroc@hotmail.com>
2022-02-14 08:57:00 +0100

    trabajando en los tests

--- src/test/java/com/econotravel/api/Bftp2EconotravelServerApplicationTests.java
+++ src/test/java/com/econotravel/api/Bftp2EconotravelServerApplicationTests.java
@@ -40,21 +40,22 @@ class Bftp2EconotravelServerApplicationTests {
         mockMvc.perform(get("/api/experiences"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[*]", hasSize(2)))
-                .andExpect(jsonPath("$[0].name", equalTo("Paseo por el Montseny")))
-                .andExpect(jsonPath("$[1].name", equalTo("Visita a la sagrada familia")))
+                .andExpect(jsonPath("$[0].name", equalTo("Paseo en bicicleta por el Montseny")))
+                .andExpect(jsonPath("$[1].name", equalTo("Descubre la Barcelona Modernista de noche")))
                 .andDo(print());
     }
 
     private void addSampleExperiences() {
         List<Experience> experiences = List.of(
-                new Experience("Paseo por el Montseny", "Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny.", "Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny. Una escapada veraniega perfecta para parejas, " +
+                new Experience("Paseo en bicicleta por el Montseny", "Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny.", "Disfruta de un hermoso paseo en bicicleta por el increíble Parque Natural del Montseny. Una escapada veraniega perfecta para parejas, " +
                         "familias y amigos que nos permitirá conocer nuevos y sorprendentes lugares. El recorrido que os proponemos comienza con una larga subida con algunos descansos, combinando " +
                         "asfalto y pistas anchas, culminando en un mirador con magníficas vistas del Vallés y el mar. Desde aquí continuaremos la bajada combinando senderos, pistas y algún tramo de carretera " +
                         "y terreno mixto para visitar la zona de Santa Fe, donde descubriremos la Ermita y el Bosque de las Secuoyas. Para finalizar, acudiremos al restaurante María Rosa de Palautordera, donde disfrutaremos " +
                         "de una excelente comida casera con butifarra blanca y negra y munxetes del Montseny.",250, 5,
                         "Montaña, bicicleta, excursión larga.", "https://cdn2.civitatis.com/espana/viladrau/tour-bicicleta-electrica-parque-montseny-grid.jpg"),
 
-                new Experience("Descubre la Barcelona Modernista de noche", "La mejor forma de descubrir las maravillas modernistas que se esconden en el barcelonés distrito del Eixample","Desplazarse a pie es una de las mejores formas de descubrir las maravillas modernistas que se esconden en el barcelonés distrito del Eixample, " +
+                new Experience("Descubre la Barcelona Modernista de noche", "La mejor forma de descubrir las maravillas modernistas que se esconden en el barcelonés distrito del Eixample",
+                        "Desplazarse a pie es una de las mejores formas de descubrir las maravillas modernistas que se esconden en el barcelonés distrito del Eixample, " +
                         "ubicado en el centro de la ciudad. " +
                         "En esta excursión de cuatro horas, descubriremos los principales emblemas del modernismo y visitaremos los templos y " +
                         "edificios más célebres del arquitecto Gaudí. \n" +
@@ -71,20 +72,20 @@ class Bftp2EconotravelServerApplicationTests {
 
         mockMvc.perform(post("/api/experiences")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\": \"Paseo en Bici por el Montseny\"}")
+                .content("{\"name\": \"Paseo en bicicleta por el Montseny\"}")
         ).andExpect(status().is(200));
 
         var experiences = experienceRepository.findAll();
 
         assertThat(experiences, contains(
-                hasProperty("name", is("Paseo en Bici por el Montseny"))
+                hasProperty("name", is("Paseo en bicicleta por el Montseny"))
         ));
     }
 
     @Test
     void allowsToCreateANewExperience() throws Exception {
-        mockMvc.perform(post("/experiences/new")
-                        .param("name", "Paseo en Bici por el Montseny")
+        mockMvc.perform(post("/new")
+                        .param("name", "Paseo en bicicleta por el Montseny")
                         .param("price", String.valueOf(250))
                         .param("time", String.valueOf(5))
                         .param("category", "Montaña, bicicleta, excursión larga")
@@ -95,10 +96,33 @@ class Bftp2EconotravelServerApplicationTests {
 
         List<Experience> existingExperiences = (List<Experience>) experienceRepository.findAll();
         assertThat(existingExperiences, contains(allOf(
-                hasProperty("name", equalTo("Paseo en bici por el Montseny")),
+                hasProperty("name", equalTo("Paseo en bicicleta por el Montseny")),
                 hasProperty("price", equalTo(250)),
                 hasProperty("time", equalTo(5)),
-                hasProperty("categoria", equalTo("Montaña, bicileta, excursión larga"))
+                hasProperty("category", equalTo("Montaña, bicicleta, excursión larga")),
+                hasProperty("imageUrl", equalTo(""))
+        )));
+    }
+
+    @Test
+    void allowsToEditExistentExperience() throws Exception {
+        mockMvc.perform(post("/api/experiences/edit")
+                        .param("name", "Paseo en bicicleta por el Montseny")
+                        .param("price", String.valueOf(250))
+                        .param("time", String.valueOf(5))
+                        .param("category", "Montaña, bicicleta, excursión larga")
+                        .param("imageUrl", "https://cdn2.civitatis.com/espana/viladrau/tour-bicicleta-electrica-parque-montseny-grid.jpg")
+
+                )
+                .andExpect(status().is(200));
+
+        List<Experience> existingExperiences = (List<Experience>) experienceRepository.findAll();
+        assertThat(existingExperiences, contains(allOf(
+                hasProperty("name", equalTo("Paseo en bicicleta por el Montseny")),
+                hasProperty("price", equalTo(250)),
+                hasProperty("time", equalTo(5)),
+                hasProperty("category", equalTo("Montaña, bicicleta, excursión larga")),
+                hasProperty("imageUrl", equalTo("https://cdn2.civitatis.com/espana/viladrau/tour-bicicleta-electrica-parque-montseny-grid.jpg"))
         )));
     }
 
