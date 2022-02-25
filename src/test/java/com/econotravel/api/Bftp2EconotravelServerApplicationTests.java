a69ba6aa64bc85e7d9302596806682053d4a6e71
RosaPs <rosapacheco.contact@gmail.com>
2022-02-25 10:15:35 +0100

    Añadido archivo, no veo home

--- src/test/java/com/econotravel/api/Bftp2EconotravelServerApplicationTests.java
+++ src/test/java/com/econotravel/api/Bftp2EconotravelServerApplicationTests.java
@@ -15,7 +15,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
